ace335b161ef607da6f62b9f66e2affab5d30731
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-03-29 17:54:22 +0100

    almost finalised, everything working

--- main.js
+++ main.js
@@ -1,37 +1,35 @@
-// ------------------------------------------------------------------
-// JS using ID's
-// -------------------------------------------------------------------
-document.addEventListener("keydown", (e) => {
-  let viewDisplay = document.getElementById("displayKeyNum");
-  let viewCode = document.getElementById("displayCode");
-  let viewKey = document.getElementById("displayKey");
-  let viewWhich  = document.getElementById("displayWhich");
-
-  let keyDown = e.key;
-  let keyCode = e.code;
-  let keyWhich = e.which;
-
-viewDisplay.innerHTML = `<p class="pressedBtn"> ${keyDown} was pressed </p>`
-viewKey.innerHTML = `<p class="pressedBtn"> ${keyCode} </p>`
-viewWhich.innerHTML = `<p class="pressedBtn"> ${keyWhich} </p>`
-viewCode.innerHTML = `<p class="pressedBtn"> ${keyDown} </p>`
-})
 
 // ------------------------------------------------------------------------
 // JS using classes
 // ------------------------------------------------------------------------
+
+document.addEventListener("keydown", (e) => {
+    let viewDisplay = document.querySelector(".displayKeyNum");
+    let viewKey = document.querySelector(".displayKey");
+    let viewWhich  = document.querySelector(".displayWhich");
+    let viewCode = document.querySelector(".displayCode");
+  
+    let keyDown = e.key;
+    let keyCode = e.code;
+    let keyWhich = e.which;
+  
+  viewDisplay.innerHTML = `<p class="pressedBtn"> You pressed the: '${keyDown}' key </p>`
+  viewKey.innerHTML = `<p class="pressedBtn"> ${keyDown} </p>`
+  viewWhich.innerHTML = `<p class="pressedBtn"> ${keyWhich} </p>`
+  viewCode.innerHTML = `<p class="pressedBtn"> ${keyCode} </p>`
+  
+})
+
+// -------------------------------------------------------------------------
+// other
+// -----------------------------------------------------------------------
 // document.addEventListener("keydown", (e) => {
-//     let viewDisplay = document.getElementByClassName("displayKeyNum");
-//     let viewCode = document.getElementByClassName("displayCode");
-//     let viewKey = document.getElementByClassName("displayKey");
-//     let viewWhich  = document.getElementByClassName("displayWhich");
-  
-//     let keyDown = e.key;
-//     let keyCode = e.code;
-//     let keyWhich = e.which;
-  
-//   viewDisplay.innerHTML = `<p class="pressedBtn"> ${keyDown} was pressed </p>`
-//   viewKey.innerHTML = `<p class="pressedBtn"> ${keyCode} </p>`
-//   viewWhich.innerHTML = `<p class="pressedBtn"> ${keyWhich} </p>`
-//   ViewCode.innerHTML = `<p class="pressedBtn"> ${keyDown} </p>`
+//   let descriptions = document.getElementByClassName("description");
+//   let keyDown = e.key;
+//   let keyCode = e.code;
+//   let keyWhich = e.which;
+
+// descriptions[0].innerHTML = `<p class="pressedBtn"> ${keyDown} was pressed </p>`
+// descriptions[1].innerHTML = `<p class="pressedBtn"> ${keyCode} </p>`
+// descriptions[2].innerHTML = `<p class="pressedBtn"> ${keyWhich} </p>`
 // })
